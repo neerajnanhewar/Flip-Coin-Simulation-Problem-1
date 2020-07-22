e1b6ac746727452e1167da3be32f9e96313976a4
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-07-22 15:45:36 +0530

    Added : Initial file added

---
+++ flipCoinSim_Win_Tie_loss.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+#Program to flip a coin [H,T]
+heads=1
+tails=0
+function flipCoin
+{
+	flip=$((RANDOM%2))
+	if [ $flip -eq $heads ]
+	then
+			echo "Head Wins"
+	elif [ $flip -eq $tails ]
+	then
+			echo "Tail Wins"
+	else
+			exit
+	fi
+}
+for ((i=0;i<=20;i++))
+do
+flipCoin
+done
