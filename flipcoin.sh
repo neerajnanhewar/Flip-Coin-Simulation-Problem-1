7c7fa41b1f60a01102b05902ea315752184c6d87
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-07-22 15:17:51 +0530

    Refactore : flipCoin Function Added

--- flipcoin.sh
+++ flipcoin.sh
@@ -2,13 +2,18 @@
 #Program to flip a coin [H,T]
 heads=1
 tails=0
-flip=$((RANDOM%2))
-if [ $flip -eq $heads ]
-then
-		echo "Head Wins"
-elif [ $flip -eq $tails ]
-then
-		echo "Tail Wins"
-else
-		exit
-fi
+function flipCoin
+{
+	flip=$((RANDOM%2))
+	if [ $flip -eq $heads ]
+	then
+			echo "Head Wins"
+	elif [ $flip -eq $tails ]
+	then
+			echo "Tail Wins"
+	else
+			exit
+	fi
+}
+
+flipCoin
