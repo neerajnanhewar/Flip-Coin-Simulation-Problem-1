325b2bc04df920bc999e3dbe48d4ad93969885eb
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-07-22 16:06:09 +0530

    Added: Who_Win Function added

--- flipCoinSim_Win_Tie_loss.sh
+++ flipCoinSim_Win_Tie_loss.sh
@@ -1,21 +1,46 @@
-#!/bin/bash -x
+#!/bin/bash 
 #Program to flip a coin [H,T]
 heads=1
 tails=0
+head_win=0
+tail_win=0
 function flipCoin
 {
-	flip=$((RANDOM%2))
-	if [ $flip -eq $heads ]
+	while [ $head_win -ne 21 ] && [ $tail_win -ne 21 ]
+	do
+		flip=$((RANDOM%2))
+		if [ $flip -eq $heads ]
+		then
+				echo "Heads"
+				((head_win++))
+		elif [ $flip -eq $tails ]
+		then
+				echo "Tails"
+				((tail_win++))
+		else
+				echo "It's a Tie " 
+		fi
+	done
+}
+
+function  who_Win
+{
+	if [ $head_win -gt $tail_win ]
 	then
-			echo "Head Wins"
-	elif [ $flip -eq $tails ]
+			wonTime=$(($head_win-$tail_win))
+			echo "Head wins" $wonTime "Times"
+	elif [ $tail_win -gt $head_win ]
 	then
-			echo "Tail Wins"
+			wonTime=$(($tail_win-$head_win))
+			echo "Tail wins" $wonTime "Times"
 	else
-			exit
+			echo "It's a Tie"
 	fi
 }
-for ((i=0;i<=20;i++))
-do
+
 flipCoin
-done
+echo $head_win
+echo $tail_win
+who_Win
+
+
